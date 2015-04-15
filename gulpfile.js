cc42d423be899803d3a846142a1d8f45ce8b625f
Justin Smith <justin.smith@summitsystemsinc.com>
2015-04-15 16:53:18 -0500

    Fixing merge

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,8 @@
 
 
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+var modRewrite = require('connect-modrewrite');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
 var jsReporter = require('jshint-stylish');
@@ -31,31 +33,65 @@ var annotateAdfPlugin = require('ng-annotate-adf-plugin');
 var pkg = require('./package.json');
 var name = pkg.name;
 
+var templateOptions = {
+  root: '../src/templates',
+  module: 'adf'
+};
+
 var annotateOptions = {
   plugin: [
     annotateAdfPlugin
   ]
 };
 
+var minifyHtmlOptions = {
+  empty: true,
+  loose: true
+};
+
+var protractorOptions = {
+  configFile: 'protractor.conf.js'
+};
+
 /** lint **/
 
-gulp.task('lint', function(){
-  gulp.src('src/*.js')
+gulp.task('csslint', function(){
+  gulp.src('src/styles/*.css')
+      .pipe($.csslint())
+      .pipe($.csslint.reporter());
+});
+
+gulp.task('jslint', function(){
+  gulp.src('src/scripts/*.js')
       .pipe($.jshint())
       .pipe($.jshint.reporter(jsReporter));
 });
 
+gulp.task('lint', ['csslint', 'jslint']);
+
 /** clean **/
 
 gulp.task('clean', function(cb){
-  del('dist', cb);
+  del(['dist', '.tmp'], cb);
 });
 
 /** build **/
 
-gulp.task('build', function(){
-  gulp.src('src/*.js')
+gulp.task('css', function(){
+  gulp.src('src/styles/*.css')
+      .pipe($.concat(name + '.css'))
+      .pipe(gulp.dest('dist/'))
+      .pipe($.rename(name + '.min.css'))
+      .pipe($.minifyCss())
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('js', function(){
+  gulp.src(['src/scripts/*.js', 'src/templates/*.html'])
+      .pipe($.if('*.html', $.minifyHtml(minifyHtmlOptions)))
+      .pipe($.if('*.html', $.angularTemplatecache(name + '.tpl.js', templateOptions)))
       .pipe($.sourcemaps.init())
+      .pipe($.if('*.js', $.replace('<<adfVersion>>', pkg.version)))
       .pipe($.concat(name + '.js'))
       .pipe($.ngAnnotate(annotateOptions))
       .pipe(gulp.dest('dist/'))
@@ -65,5 +101,162 @@ gulp.task('build', function(){
       .pipe(gulp.dest('dist/'));
 });
 
-/** default */
+gulp.task('build', ['css', 'js']);
+
+/** build docs **/
+
+gulp.task('docs', function(){
+  return gulp.src('src/scripts/*.js')
+    .pipe($.ngdocs.process({}))
+    .pipe(gulp.dest('./dist/docs'));
+});
+
+/** build sample **/
+gulp.task('install-widgets', function(){
+  return gulp.src('sample/widgets/*/bower.json')
+             .pipe($.install());
+});
+
+gulp.task('widget-templates', ['install-widgets'], function(){
+  var opts = {
+    root: '{widgetsPath}',
+    module: 'sample'
+  };
+  return gulp.src('sample/widgets/*/src/*.html')
+             .pipe($.minifyHtml(minifyHtmlOptions))
+             .pipe($.angularTemplatecache('widgets.js', opts))
+             .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('sample-templates', function(){
+  var opts = {
+    root: 'partials',
+    module: 'sample'
+  };
+  return gulp.src('sample/partials/*.html')
+             .pipe($.minifyHtml(minifyHtmlOptions))
+             .pipe($.angularTemplatecache('samples.js', opts))
+             .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('dashboard-templates', function(){
+  var opts = {
+    root: '../src/templates',
+    module: 'adf'
+  };
+  return gulp.src('src/templates/*.html')
+             .pipe($.minifyHtml(minifyHtmlOptions))
+             .pipe($.angularTemplatecache('adf.js', opts))
+             .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('copy-font', function(){
+  gulp.src('sample/components/bootstrap/dist/fonts/*')
+      .pipe(gulp.dest('dist/sample/fonts'));
+});
+
+gulp.task('sample', ['widget-templates', 'sample-templates', 'dashboard-templates', 'copy-font'], function(){
+  var templates = gulp.src('.tmp/*.js', {read: false});
+  var assets = $.useref.assets();
+  gulp.src('sample/index.html')
+      // inject templates
+      .pipe($.inject(templates, {relative: true}))
+      .pipe(assets)
+      .pipe($.if('*.js', $.replace('<<adfVersion>>', pkg.version)))
+      .pipe($.if('*.js', $.ngAnnotate(annotateOptions)))
+      .pipe($.if('*.js', $.uglify()))
+      .pipe($.if('*.css', $.minifyCss()))
+      .pipe($.rev())
+      .pipe(assets.restore())
+      .pipe($.useref())
+      .pipe($.revReplace())
+      .pipe(gulp.dest('dist/sample'));
+});
+
+/** livereload **/
+
+gulp.task('watch', function(){
+  var paths = [
+    'src/scripts/*.js',
+    'src/styles/*.css',
+    'src/templates/*.html',
+    'sample/*.html',
+    'sample/scripts/*.js',
+    'sample/partials/*.html',
+    'sample/widgets/*/*.js',
+    'sample/widgets/*/*.css',
+    'sample/widgets/*/*.html',
+    'sample/widgets/*/src/*.js',
+    'sample/widgets/*/src/*.css',
+    'sample/widgets/*/src/*.html'
+  ];
+  gulp.watch(paths).on('change', function(file){
+    gulp.src(file.path)
+        .pipe(connect.reload());
+  });
+});
+
+gulp.task('webserver', ['install-widgets'], function(){
+  connect.server({
+    port: 9001,
+    livereload: true,
+    // redirect / to /sample
+    middleware: function() {
+      return [
+        modRewrite([
+          '^/$ /sample/ [R]'
+        ])
+      ];
+    }
+  });
+});
+
+gulp.task('serve', ['webserver', 'watch']);
+
+/** e2e **/
+
+// The protractor task
+var protractor = require('gulp-protractor').protractor;
+
+// Start a standalone server
+var webdriver_standalone = require('gulp-protractor').webdriver_standalone;
+
+// Download and update the selenium driver
+var webdriver_update = require('gulp-protractor').webdriver_update;
+
+// Downloads the selenium webdriver
+gulp.task('webdriver_update', webdriver_update);
+
+// Start the standalone selenium server
+// NOTE: This is not needed if you reference the
+// seleniumServerJar in your protractor.conf.js
+gulp.task('webdriver_standalone', webdriver_standalone);
+
+// start webserver for e2e tests
+gulp.task('e2e-server', ['install-widgets'], function(){
+  connect.server({
+    port: 9003
+  });
+});
+
+// Setting up the test task
+gulp.task('e2e', ['e2e-server', 'webdriver_update'], function(cb) {
+  gulp.src('e2e/*Spec.js')
+      .pipe(protractor(protractorOptions))
+      .on('error', function(e) {
+        // stop webserver
+        connect.serverClose();
+        // print test results
+        console.log(e);
+      })
+      .on('end', function(){
+        // stop webserver
+        connect.serverClose();
+        cb();
+      });
+});
+
+/** shorthand methods **/
+gulp.task('all', ['build', 'docs', 'sample']);
+
 gulp.task('default', ['build']);
